c59ef18c41f3d38d1cb7981117be6d74df25b085
andrejw27 <45046071+andrejw27@users.noreply.github.com>
2021-07-31 23:43:48 +0200

    Update model.py

--- model.py
+++ model.py
@@ -1,7 +1,8 @@
 import torch
 from torch import nn
-
 import torch.nn.functional as F
+
+#Reference: https://github.com/milesial/Pytorch-UNet
 
 class UNET(nn.Module):
     def __init__(self, in_channels, out_channels):
@@ -16,7 +17,7 @@ class UNET(nn.Module):
         self.output = self.out(16, out_channels)
 
     def __call__(self, x, layer_name=None):
-        
+        # if you want to obtain output of certain layer, just pass the desired output layer in the layer_name 
         # downsampling part
         x = x.float()
         conv1 = self.conv1(x)
@@ -99,56 +100,3 @@ class UNET(nn.Module):
         
         return out
     
-
-class UNET_old(nn.Module):
-    def __init__(self, in_channels, out_channels):
-        super().__init__()
-
-        self.conv1 = self.contract_block(in_channels, 16, 3, 1)
-        self.conv2 = self.contract_block(16, 32, 3, 1)
-        self.conv3 = self.contract_block(32, 64, 3, 1)
-
-        self.upconv3 = self.expand_block(64, 32, 3, 1)
-        self.upconv2 = self.expand_block(32*2, 16, 3, 1)
-        self.upconv1 = self.expand_block(16*2, out_channels, 3, 1)
-
-    def __call__(self, x):
-
-        # downsampling part
-        x = x.float()
-        conv1 = self.conv1(x)
-        conv2 = self.conv2(conv1)
-        conv3 = self.conv3(conv2)
-
-        #upsampling part
-        upconv3 = self.upconv3(conv3)
-        upconv2 = self.upconv2(torch.cat([upconv3, conv2], 1))
-        upconv1 = self.upconv1(torch.cat([upconv2, conv1], 1))
-
-        return upconv1
-
-    def contract_block(self, in_channels, out_channels, kernel_size, padding):
-
-        contract = nn.Sequential(
-            torch.nn.Conv2d(in_channels, out_channels, kernel_size=kernel_size, stride=1, padding=padding),
-            torch.nn.BatchNorm2d(out_channels),
-            torch.nn.ReLU(),
-            torch.nn.Conv2d(out_channels, out_channels, kernel_size=kernel_size, stride=1, padding=padding),
-            torch.nn.BatchNorm2d(out_channels),
-            torch.nn.ReLU(),
-            torch.nn.MaxPool2d(kernel_size=3, stride=2, padding=1)
-                                 )
-
-        return contract
-
-    def expand_block(self, in_channels, out_channels, kernel_size, padding):
-
-        expand = nn.Sequential(torch.nn.Conv2d(in_channels, out_channels, kernel_size, stride=1, padding=padding),
-                            torch.nn.BatchNorm2d(out_channels),
-                            torch.nn.ReLU(),
-                            torch.nn.Conv2d(out_channels, out_channels, kernel_size, stride=1, padding=padding),
-                            torch.nn.BatchNorm2d(out_channels),
-                            torch.nn.ReLU(),
-                            torch.nn.ConvTranspose2d(out_channels, out_channels, kernel_size=3, stride=2, padding=1, output_padding=1) 
-                            )
-        return expand
